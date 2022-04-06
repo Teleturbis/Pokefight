59042ae834ce843860ea0ff0c89e7bf8e05507cf
Kevin Poppe <kevin.poppe93@gmail.com>
2022-04-06 17:11:00 +0200

    added chat

--- src/components/main/Chat.jsx
+++ src/components/main/Chat.jsx
@@ -1,50 +1,56 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
+
+import { io } from "socket.io-client";
 
 export default function Chat() {
-  const [messages, setMessages] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const socket = useRef();
+
+  const date = new Date();
 
   useEffect(() => {
-    setMessages(
-      [
-        {
-          timestamp: "13:15",
-          content: "Hey @All. How's it going?",
-        },
-        {
-          timestamp: "13:17",
-          content: "Good! And u?",
-        },
-        {
-          timestamp: "13:22",
-          content: "Some have a Pokeball for me?",
-        },
-        {
-          timestamp: "13:23",
-          content: "Yeah sure. Come over!",
-        },
-        {
-          timestamp: "13:27",
-          content:
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id omnis repellendus tempora? Quis illo, qui eligendi soluta aut labore. Velit ratione provident cumque corporis suscipit ut id deleniti nesciunt neque!",
-        },
-      ].reverse()
-    );
+    socket.current = io("https://express-db-pokefight.herokuapp.com");
+
+    socket.current.on("connect", () => {
+      console.log("client connected", socket.current.id);
+    });
+
+    socket.current.on("msg-received", (message) => {
+      setMessages((prev) => [message, ...prev]);
+    });
+
+    return () => {
+      socket.current.disconnect();
+    };
   }, []);
+
+  function handleSendMsg() {
+    socket.current.emit("msg-event", userInput);
+    setUserInput("");
+  }
 
   return (
     <div className="chat-main-div">
       <div className="chat-messages-div">
-        {messages &&
-          messages.map((message, index) => (
-            <div className="chat-message" key={index}>
-              <p className="chat-message-timestamp">{message.timestamp}</p>
-              <p className="chat-message-content">{message.content}</p>
-            </div>
-          ))}
+        {messages.map((message, index) => (
+          <div className="chat-message" key={index}>
+            <p className="chat-message-content">{message}</p>
+          </div>
+        ))}
       </div>
       <div className="chat-userinput">
-        <input type="text" className="chat-userinput-input" />
-        <button className="chat-userinput-button chat-userinput-input">
+        <input
+          type="text"
+          className="chat-userinput-input"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <button
+          className="chat-userinput-button chat-userinput-input"
+          onClick={() => handleSendMsg()}
+        >
           Send
         </button>
       </div>
--- src/components/main/InGame.jsx
+++ src/components/main/InGame.jsx
@@ -5,8 +5,20 @@ import { FiMenu } from "react-icons/fi";
 import Chat from "./Chat";
 import Inventar from "../gamElements/Inventar";
 
+const map = require("../../assets/unbenannt.png");
+
 export default function MainMenu({ user, changeUser }) {
   const [inventaryVisible, setInventaryVisible] = useState(false);
+  const [mapVisible, setMapVisible] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  function handleDelete() {}
+
+  function handleChangeName() {}
+
+  function handleLogout() {
+    changeUser({ username: "", token: "", loggedIn: false });
+  }
 
   return (
     <div className="main-game-div">
@@ -14,18 +26,53 @@ export default function MainMenu({ user, changeUser }) {
       <div className="game-overlay">
         <GiSchoolBag
           className="inv-btn"
-          onClick={() => setInventaryVisible(!inventaryVisible)}
+          onClick={() => {
+            setInventaryVisible(!inventaryVisible);
+            setMapVisible(false);
+            setMenuVisible(false);
+          }}
         />
         <div className="information-div">
-          <FiMenu className="menu-btn" />
+          <FiMenu
+            className="menu-btn"
+            onClick={() => setMenuVisible(!menuVisible)}
+          />
           <p className="level">Level 99</p>
           <p className="money">
             <GiTwoCoins className="money-symbole" /> 999$
           </p>
         </div>
-        <GiTreasureMap className="map-btn" />
-        {inventaryVisible ? <Inventar /> : null}
+        <GiTreasureMap
+          className="map-btn"
+          onClick={() => {
+            setMapVisible(!mapVisible);
+            setInventaryVisible(false);
+            setMenuVisible(false);
+          }}
+        />
         <Chat />
+
+        {mapVisible ? (
+          <div className="game-map-div">
+            <img src={map} className="game-map" />
+          </div>
+        ) : null}
+
+        {inventaryVisible ? <Inventar /> : null}
+
+        {menuVisible ? (
+          <div>
+            <div>
+              <h1>Menu</h1>
+              <h2>Hey {user.userName}!</h2>
+              <button onClick={() => handleLogout()}>Log Out</button>
+              <button onClick={() => handleDelete()}>Delete Account</button>
+              <button onClick={() => handleChangeName()}>
+                Change Username
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
